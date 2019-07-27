459a3e71cd28851b901db57a7b9ee875fb7d91da
Alan Turing <freemasl.sf@gmail.com>
2019-07-26 18:36:10 -0600

    Add methods to update main tab on the DOM

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -11,11 +11,39 @@ let domUpdates = {
   },
 
   displayCurrentDate() {
-    let today = new Date();
-    let day = String(today.getDate()).padStart(2, '0');
-    let month = String(today.getMonth() + 1).padStart(2, '0');
-    let year = today.getFullYear();
-    return `${month}/${day}/${year}`;
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    let currentDate = new Date();
+    let displayDate =
+    days[currentDate.getDay()] +
+    " - " +
+    months[currentDate.getMonth()] +
+    " " +
+    currentDate.getDate() +
+    ", " +
+    currentDate.getFullYear();
+    return displayDate;
   },
 
 }

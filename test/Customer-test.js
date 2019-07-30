5ce95e417953872412470372594b3ad58d448ab3
Alan Turing <freemasl.sf@gmail.com>
2019-07-30 16:25:09 -0600

    Add missing chai spies to tests

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -1,7 +1,17 @@
 import chai from 'chai';
+const spies = require('chai-spies');
+chai.use(spies);
 const expect = chai.expect;
 import Customer from '../src/Customer';
 import Hotel from '../src/Hotel';
+import domUpdates from '../src/dom-updates';
+import bookings from '../src/data/bookings-data';
+import rooms from '../src/data/rooms-data';
+import roomServices from '../src/data/roomServices-data';
+import users from '../src/data/users-data';
+
+chai.spy.on(domUpdates, ['addRoomBill', 'addRoomService'], () => { });
+
 
 describe('Customer', () => {
   let customer;
@@ -36,7 +46,7 @@ describe('Customer', () => {
         totalCost: 5.5
       }
     ];
-    hotel = new Hotel();
+    hotel = new Hotel(users, bookings, roomServices, rooms);
     customer = new Customer(name, id, bookings, roomServices, hotel);
   });
 
@@ -58,6 +68,16 @@ describe('Customer', () => {
 
   it('should sort Bookings by date', () => {
     expect(customer.sortDateBookings()[0].date).to.eql("2019/08/24");
+  });
+
+  it('should add a new list of Room Services to the DOM with the updated array', () => {
+    customer.createSelectedRoomService('2019/08/24', 'sandwich', 5.99);
+    expect(domUpdates.addRoomService).to.have.been.called(1); 
+  });
+
+  it('should add a new Room Bill to the DOM with the updated array', () => {
+    customer.createSelectedBooking('2019/08/24', 1);
+    expect(domUpdates.addRoomBill).to.have.been.called(1);
   });
 
 });
